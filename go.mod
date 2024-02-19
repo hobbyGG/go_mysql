60edddb072e6c9af249e5a6673c13f3773270ffb
hobbyGG <hobbyGG@outlook.com>
2024-02-19 12:09:17 +0800

    mod

--- go.mod
+++ go.mod
@@ -2,10 +2,4 @@ module src
 
 go 1.19
 
-require (
-	github.com/denisenkom/go-mssqldb v0.12.3 // indirect
-	github.com/go-sql-driver/mysql v1.7.1 // indirect
-	github.com/golang-sql/civil v0.0.0-20190719163853-cb61b32ac6fe // indirect
-	github.com/golang-sql/sqlexp v0.1.0 // indirect
-	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
-)
+require github.com/go-sql-driver/mysql v1.7.1
--- src/main.go
+++ src/main.go
@@ -11,12 +11,20 @@ import (
 
 var db *sql.DB //数据库
 
+const (
+	user_name = "root"
+	password  = "1123"
+	ip        = "127.0.0.1"
+	port      = "6666"
+	database  = "godb"
+)
+
 func main() {
-	connStr := "root:1123@tcp(127.0.0.1:6666)/godb" //连接字符串
+	connStr := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user_name, password, ip, port, database) //连接字符串
 
 	db, err := sql.Open("mysql", connStr) //数据库驱动，连接字符串
 	if err != nil {
-		log.Fatalln(err.Error())
+		log.Fatalln(err.Error())	//Fatalln就是println再exit
 	}
 
 	ctx := context.Background() /*需要一个上下文类型的参数，context可以携带请求范围的值
